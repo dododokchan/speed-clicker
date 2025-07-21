2a58dd971b24bab3cd5446dce4c20c06c22d27f6
doi <183935098+dododokchan@users.noreply.github.com>
2025-07-21 17:10:48 +0900

    adjust tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+
+            keyframes: {
+                "run-in-place": {
+                    "0%, 100%": { transform: "translateX(0)" },
+                    "25%": { transform: "translateX(-3px)" },
+                    "75%": { transform: "translateX(3px)" },
+                },
+            },
+            animation: {
+                "run-in-place": "run-in-place 0.4s steps(2) infinite",
             },
         },
     },
