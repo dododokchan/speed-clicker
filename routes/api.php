282d5bb6f5f89abaafad04ebe8e7877ec1362e47
doi <183935098+dododokchan@users.noreply.github.com>
2025-07-27 00:53:27 +0900

    feat(api): protect /api/scores with sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ScoreController;
 
 //Route::post('/scores', [ScoreController::class, 'store']);
+
+Route::middleware('auth:sanctum')->post('/scores', [ScoreController::class, 'store'])
+    ->name('scores.store');
